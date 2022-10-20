58aa590e6df362ed999e3392c4b875f7dcc997d7
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-10-20 19:44:22 +0100

    Updates

---
+++ README.md
@@ -0,0 +1,27 @@
+# Installette <img src="https://github.com/devicons/devicon/blob/master/icons/bash/bash-original.svg" title="Bash" alt="Bash Logo" width="40" height="50" align="right" />&nbsp;
+
+## About Installette
+Installette is a shell script developed for Linux that aims to be a
+simplified and automated setup for the 42 environment.
+
+### Installing and running installette:
+1- Clone this repository
+
+	git clone https://github.com/Kuninoto/installette  
+2- Navigate to _installette_
+
+	cd installette  
+3- Run _installette.sh_ with your desired options.
+
+#### Available Options:
+`-v` - Installs Vim  
+`-h` - Installs 42Header for Vim  
+`-n` - Installs Norminette  
+`-c` - Unistalls 42Header  
+
+#### 42Header.vim file
+[42header.vim](https://github.com/gcamerli/42header)
+
+---
+Made by Nuno Carvalho (Kuninoto) | nnuno-ca@student.42porto.com  
+<div id="badge"> <a href="https://www.linkedin.com/in/nuno-carvalho-218822247"/> <img src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn Badge"/>&nbsp;
---
+++ installette.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# REQUIREMENTS
+#	BASH	
+
+# SOURCES
+#	https://github.com/gcamerli/42header
+
+# Author: Nuno Carvalho (Kuninoto)
+# LINUX
+
+# ANSI COLOR CODES
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+WHITE='\033[0;37m'
+RESET='\033[0m'
+
+# UPDATE && UPGRADE SYSTEM
+notify-send "Updating Your System. This may take some time..."
+sudo apt update
+sudo apt upgrade -y
+
+# PYTHON3 & PIP CHECK
+if ! [ -x "$(command -v python3)" ]; then
+  echo -e "${RED}Error${RESET}: python3 is not installed."
+  echo -e "Proceeding to python3 installation..."
+  sudo apt-get install python3
+fi
+
+if ! [ -x "$(command -v pip)" ]; then
+  echo -e "${RED}Error${RESET}: pip is not installed."
+  echo "Proceeding to pip installation..."
+  sudo apt-get install python3-pip
+fi
+
+for arg in "$@"
+do
+	case "$arg" in
+		"-v"*)	if [ -x "$(command -v vim)" ]; then
+					echo "Vim is already installed."
+				else
+					echo "${CYAN}Installing Vim"
+					sudo apt-get install vim
+				fi;;
+
+		"-h"*)	if [ -x "$(command -v vim)" ]; then
+					echo -e -n "${CYAN}Please insert your 42username${RESET}: "
+					read -r FT_User
+
+					if ! grep -q 'MAIL=' "/home/$(whoami)/.bashrc"; then
+
+					{	echo "USER=$FT_User"
+						echo "MAIL=$FT_User@student.42.fr"
+						echo "export MAIL"; } >> ~/.bashrc
+					
+					fi
+					# mkdir with -p option creates folder if it doesnt exist, if it exists, does nothing.
+					mkdir -p ~/.vim/plugin
+					cp stdheader.vim ~/.vim/plugin/
+					echo "nmap <f1> :FortyTwoHeader<CR>" >> ~/.vimrc
+				else
+					echo "${RED}Error${RESET}: vim is not installed."
+					echo "Run norminette_installer with -v option"
+				fi;;
+
+		"-n"*)	if [ -x "$(command -v norminette)" ]; then
+					echo "Norminette is already installed."
+				else
+					echo -e "${CYAN}Installing Norminette"
+					python3 -m pip install --upgrade pip setuptools
+					python3 -m pip install norminette
+					echo "Setting norminette path on PATH env variable"
+					echo "export PATH=$PATH: /home/$(whoami)/.local/bin/">> ~/.bashrc
+				fi;;
+
+		"-u"*)  sed -i '/^USER/d' ~/.bashrc
+				sed -i '/^MAIL/d' ~/.bashrc
+				sed -i '/export MAIL/d' ~/.bashrc
+				sed -i '/nmap <f1> :Forty/d' ~/.vimrc
+				rm -rf ~/.vim/plugin/stdheader.vim;;
+	esac
+done
+
+echo -e "\n${WHITE}Installette terminated."
+echo -e "Thanks for using Installette =)"
+echo -e "Yours truly, ${CYAN}Nuno Carvalho (Kuninoto)\n${WHITE}nnuno-ca@42porto.student.com${RESET}"
